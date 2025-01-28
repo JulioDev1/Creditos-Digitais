84135628133192dd2d1e0dcdbad4c8e12ccb9463
Julio Cesar <juliocesargualberto617@gmail.com>
2025-01-28 19:01:12 -0300

    test: should be balance transfer

--- Carteiras-Digitais.Test/Services.Tests/TransactionServiceTest.cs
+++ Carteiras-Digitais.Test/Services.Tests/TransactionServiceTest.cs
@@ -18,7 +18,7 @@ namespace Carteiras_Digitais.Test.Services.Tests
         public TransactionServiceTest()
         {
             this.transactionRepository = new Mock<ITransactionRepository>();
-            this.walletService = new Mock<IWalletService>();  
+            this.walletService = new Mock<IWalletService>();
             fixture = new Fixture();
             fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
             fixture.Behaviors.Add(new OmitOnRecursionBehavior());
@@ -34,7 +34,7 @@ namespace Carteiras_Digitais.Test.Services.Tests
                 .With(x => x.Amount, wallet.Balance)
                 .Create();
 
-            var userWalletNotFound = walletService.Setup((w=> w.GetUserBalanceWallet(wallet.Id))).ReturnsAsync((Wallet ? ) null);
+            var userWalletNotFound = walletService.Setup((w => w.GetUserBalanceWallet(wallet.Id))).ReturnsAsync((Wallet?)null);
 
             var service = new TransactionService(transactionRepository.Object, walletService.Object);
 
@@ -55,17 +55,16 @@ namespace Carteiras_Digitais.Test.Services.Tests
 
                 };
             var senderWallet =
-                 new Wallet
-                 {
-                     Id = Guid.NewGuid(),
-                     Balance = 0,
-                     UserId = Guid.NewGuid(),
-
-                 };
-            var balance = 
+                new Wallet
+                {
+                    Id = Guid.NewGuid(),
+                    Balance = 0,
+                    UserId = Guid.NewGuid(),
+                };
+            var balance =
                 new TransactionDto
                 {
-                    Amount =300,
+                    Amount = 300,
                     ReceiverWalletId = receiverWallet.Id,
                     SenderWalletId = senderWallet.Id,
                 };
@@ -79,6 +78,75 @@ namespace Carteiras_Digitais.Test.Services.Tests
             Func<Task> action = async () => await service.TransactionToBalanceToReceiver(balance);
 
             await action.Should().ThrowAsync<InvalidOperationException>();
+        }
+        [Fact]
+        public async Task ShouldBeSuccessAndSendMoneyToReceiver()
+        {
+            var receiverWallet = new Wallet
+            {
+                Id = Guid.NewGuid(),
+                Balance = 0,
+                UserId = Guid.NewGuid(),
+
+            };
+
+            var senderWallet = new Wallet
+            {
+                Id = Guid.NewGuid(),
+                Balance = 600,
+                UserId = Guid.NewGuid(),
+            };
+
+            var receiverWalletResult = new Wallet
+            {
+                Id = receiverWallet.Id,
+                Balance = 300,
+                UserId = receiverWallet.UserId,
+
+            };
+
+            var senderWalletResult = new Wallet
+            {
+                Id = senderWallet.Id,
+                Balance = 300,
+                UserId = senderWallet.UserId,
+            };
+
+            var balance = new TransactionDto
+            {
+                Amount = 300,
+                ReceiverWalletId = receiverWallet.Id,
+                SenderWalletId = senderWallet.Id,
+            };
+
+            var transaction = new Transaction
+            {
+                Id = Guid.NewGuid(),
+                Amount = balance.Amount,
+                ReceiverWalletId = balance.ReceiverWalletId,
+                SenderWalletId = balance.SenderWalletId,
+                CreatedAt = DateTime.UtcNow,
+            };
+
+            var decreaseUser = new BalanceDto(senderWallet.Balance, senderWallet.Id);
+
+            var increaseUser = new BalanceDto(receiverWallet.Balance, receiverWallet.Id);
+            
+            walletService.Setup((w => w.GetUserBalanceWallet(receiverWallet.Id))).ReturnsAsync(receiverWallet);
+
+            walletService.Setup((w => w.GetUserBalanceWallet(senderWallet.Id))).ReturnsAsync(senderWallet);
+
+            walletService.Setup((w=> w.DiscountBalanceToWallet(It.IsAny<BalanceDto>()))).ReturnsAsync(senderWalletResult);
+
+            walletService.Setup((w => w.DepositBalanceToWallet(It.IsAny<BalanceDto>()))).ReturnsAsync(receiverWalletResult);
+
+            transactionRepository.Setup((t=> t.CreateTransaction(transaction))).ReturnsAsync(transaction);
+
+            var service = new TransactionService(transactionRepository.Object, walletService.Object);
+
+            var action = await service.TransactionToBalanceToReceiver(balance);
+
+            action.Should().Be(balance);
         }
     }
 }
