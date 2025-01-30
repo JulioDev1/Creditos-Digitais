3fb7f47ae079f2588f3c4ea342153dd90f20a56c
Julio Cesar <juliocesargualberto617@gmail.com>
2025-01-29 21:13:19 -0300

    fix:docker file

--- Carteiras-Digitais.Infrasctruture/Migrations/20250130000949_FixBugs.Designer.cs
+++ Carteiras-Digitais.Infrasctruture/Migrations/20250130000949_FixBugs.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Carteiras_Digitais.Infrasctruture.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Carteiras_Digitais.Infrasctruture.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250130000949_FixBugs")]
+    partial class FixBugs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Carteiras-Digitais.Infrasctruture/Migrations/20250130000949_FixBugs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Carteiras_Digitais.Infrasctruture.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixBugs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "transactions",
+                type: "timestamp with time zone",
+                nullable: true,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "transactions",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true,
+                oldDefaultValueSql: "now()");
+        }
+    }
+}
